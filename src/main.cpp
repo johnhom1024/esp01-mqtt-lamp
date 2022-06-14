3594c6522deda97bb4dbfce6d04ee065d49c14a9
johnhom1024 <924358746@qq.com>
2022-06-14 23:03:44 +0800

    feat: 支持PWM

--- src/main.cpp
+++ src/main.cpp
@@ -9,8 +9,15 @@
 #define mqtt_user "admin"
 #define mqtt_password "123456qq"
 
-#define in_topic "/esp01/light/in"
-#define out_topic "/esp01/light/out"
+// 主题
+// 开关灯主题
+// const char *in_topic = "/esp01/light/in";
+// const char *out_topic = "/esp01/light/out";
+
+// 设置灯的亮度
+const char *dimmable_set_topic = "/esp01/light/dimmable/set";
+// 发送灯的亮度
+const char *dimmable_get_topic = "/esp01/light/dimmable/get";
 // Replace by 2 if you aren't enable to use Serial Monitor... Don't forget to Rewire R1 to GPIO2!
 #define in_led 3
 
@@ -64,9 +71,9 @@ void reconnect()
       delay(500);
       digitalWrite(in_led, HIGH);
 
-      client.publish(out_topic, "1");
+      client.publish(dimmable_get_topic, "255");
       // 订阅消息
-      client.subscribe(in_topic);
+      client.subscribe(dimmable_set_topic);
       delay(1000);
     }
     else
@@ -80,20 +87,34 @@ void reconnect()
   }
 }
 
-void callback(char *topic, byte *payload, unsigned int length)
+/**
+ * @description: 
+ * @param {char} *topic 消息到达的主题
+ * @param {byte} *payload 消息负载
+ * @param {int} length payload的长度
+ * @return {*}
+ */
+void callback(char *topic, byte *payload, int length)
 {
   Serial.print("Message arrived [");
   Serial.print(topic);
   Serial.print("] ");
-  for (unsigned int i = 0; i < length; i++)
+  // concat the payload into a string
+  char s_payload[length];
+  for (int i = 0; i < length; i++)
   {
-    char receivedChar = (char)payload[i];
-    Serial.print(receivedChar);
-    if (receivedChar == '0')
-      digitalWrite(in_led, LOW);
-    if (receivedChar == '1')
-      digitalWrite(in_led, HIGH);
+    s_payload[i] = (char)payload[i];
   }
+
+  // 如果主题是亮度调节
+  if (String(dimmable_set_topic).equals(topic))
+  {
+    // 将char转成int类型
+    int val = atoi(s_payload);
+    Serial.println(val);
+    analogWrite(in_led, val);
+  }
+  
   Serial.println();
 }
 
@@ -107,7 +128,8 @@ void setup()
 
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(in_led, OUTPUT);
-  digitalWrite(in_led, HIGH);
+  // 点亮LED灯
+  analogWrite(in_led, 255);
 }
 
 void loop()
